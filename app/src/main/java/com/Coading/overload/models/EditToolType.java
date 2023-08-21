60f450c85b8af084278941512d26149ec737b2fb
Priya122002 <priyagirin01@gmail.com>
2023-08-21 10:43:28 +0530

    Initial commit

---
+++ app/src/main/java/com/Coading/overload/models/EditToolType.java
@@ -0,0 +1,13 @@
+package com.Coading.overload.models;
+
+public enum EditToolType {
+    COLORFILTER,
+    ADJUST,
+    HIGHLIGHT,
+    PICTURE,
+    SIGNATURE,
+    WATERMARK,
+    TEXT,
+    OVERLAY,
+    COLOREFFECT
+}
